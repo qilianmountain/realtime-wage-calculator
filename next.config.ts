d32240ff2f7585a2a068133b41941fde135dccfa
qilianmountain <924786629@qq.com>
2026-07-10 12:35:02 +0800

    Add Cloudflare Workers build configuration

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  /* config options here */
 };
 
 export default nextConfig;
 
-import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
+import("@opennextjs/cloudflare").then((m) => m.initOpenNextCloudflareForDev());
